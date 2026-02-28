941a802ff1e71788421d058b1e668bc33300f2a1
Muhammad Fajar Ramadhan <2510631170066@student.unsika.ac.id>
2026-02-28 22:39:00 +0700

    Add files via upload

---
+++ Halo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fajar Ramadhan
+ */
+public class Halo{
+    public static void main (String[]args){
+        System.out.println("Halo Karawang");
+    }
+    
+}
